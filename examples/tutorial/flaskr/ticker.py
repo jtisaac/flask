7541d7a575f46f4a8ee6800a9b4b1c3de2eedf67
jtisaac <jtisaac@users.noreply.github.com>
2021-02-07 02:44:42 -0600

    Added code to display options info for a specific date as well

--- examples/tutorial/flaskr/ticker.py
+++ examples/tutorial/flaskr/ticker.py
@@ -13,8 +13,9 @@ from flaskr.db import get_db
 import requests
 import json
 
-bp = Blueprint("ticker", __name__)
+ticker_key_prior = None
 
+bp = Blueprint("ticker", __name__)
 
 @bp.route("/ticker", methods=('POST','GET'))
 def index():
@@ -22,24 +23,54 @@ def index():
     # TODO: Add parameters (date range)
     """Return market data for ticker entered by user."""
     json_response = ""
+    json_response_options = ""
+    
+    global ticker_key_prior
+
     if request.method == "POST":
-        ticker_key = request.form["ticker_key"]
+        ticker_key = request.form.get("ticker_key", ticker_key_prior)
+        option_date = request.form.get("option_date", None)
+
         error = None
 
         if not ticker_key:
-            error = "Ticker is required."
+            #ticker_key = 'GOOGL'
+            ticker_key = ticker_key_prior
+            if not ticker_key:
+                error = "Ticker is required."
 
         if error is not None:
             flash(error)
         
         else:
-            response = requests.get('https://sandbox.tradier.com/v1/markets/timesales',
-                params={'symbol': ticker_key, 'interval': '1min', 'start': '2021-02-01 09:30', 'end': '2021-02-05 16:00', 'session_filter': 'all'},
-                headers={'Authorization': 'Bearer sNhzFaRt7B2uRiX73um6JPDceWbm', 'Accept': 'application/json'}
-            )
-            json_response = response.json()
-            print(response.status_code)
+            if option_date is None:
+                response = requests.get('https://sandbox.tradier.com/v1/markets/timesales',
+                    params={'symbol': ticker_key, 'interval': '1min', 'start': '2021-02-01 09:30', 'end': '2021-02-05 16:00', 'session_filter': 'all'},
+                    headers={'Authorization': 'Bearer sNhzFaRt7B2uRiX73um6JPDceWbm', 'Accept': 'application/json'}
+                )
+                json_response = response.json()
+                ticker_key_prior = ticker_key
+                print(response.status_code)
 
-            print(json_response)
+                print(json_response)
+            else:
+                print("trying options")
+                response = requests.get('https://sandbox.tradier.com/v1/markets/timesales',
+                    params={'symbol': ticker_key, 'interval': '1min', 'start': '2021-02-01 09:30', 'end': '2021-02-05 16:00', 'session_filter': 'all'},
+                    headers={'Authorization': 'Bearer sNhzFaRt7B2uRiX73um6JPDceWbm', 'Accept': 'application/json'}
+                )
+                json_response = response.json()
+                print(response.status_code)
 
-    return render_template("ticker/ticker.html", ticker_info=json_response) #ticker_info=json.dumps(json_response)
+                print(json_response)
+                ticker_key_prior = ticker_key
+
+                response = requests.get('https://sandbox.tradier.com/v1/markets/options/chains',
+                    params={'symbol': ticker_key, 'expiration': option_date, 'greeks': 'true'},
+                    headers={'Authorization': 'Bearer sNhzFaRt7B2uRiX73um6JPDceWbm', 'Accept': 'application/json'}
+                )
+                json_response_options = response.json()
+                print(response.status_code)
+                print(json_response_options)
+
+    return render_template("ticker/ticker.html", ticker_info=json_response, options_info=json_response_options) #ticker_info=json.dumps(json_response)
